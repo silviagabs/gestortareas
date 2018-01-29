f3b5b4f72a5ef9ceb5c7149df700ebb05be7381c
silviaYo <silvia.gomeza@gmail.com>
2018-01-29 23:39:56 +0100

    campoFecha

--- views/tareas/_form.php
+++ views/tareas/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\jui\DatePicker;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Tareas */
@@ -18,23 +19,40 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'categoria')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'prioridad')
-            ->dropDownList([ 
-                             'BAJA' => 'BAJA', 'MEDIA' => 'MEDIA', 'ALTA' => 'ALTA', 'URGENTE' => 'URGENTE' 
-                            ], 
-                            ['prompt'=>'Selecciona una Prioridad'] ); ?>
+    <?=
+            $form->field($model, 'prioridad')
+            ->dropDownList([
+                'BAJA' => 'BAJA', 'MEDIA' => 'MEDIA', 'ALTA' => 'ALTA', 'URGENTE' => 'URGENTE'
+                    ], ['prompt' => 'Selecciona una Prioridad']);
+    ?>
+    <div class="row">
+        <div class="col-lg-offset-2 col-lg-5">
+    <?=
+    $form->field($model, 'fecha_inicio')->widget(
+            DatePicker::className(), [
+        // inline too, not bad
+        'inline' => true,
+                'language'=>'es',
+        'dateFormat' => 'yyyy-MM-dd'
+    ]);
+    ?>
+            </div>
+<div class="col-lg-5">
+    <?=
+    $form->field($model, 'fecha_fin')->textInput()->widget(DatePicker::className(), [
+        'inline' => true,
+        'dateFormat' => 'yyyy-MM-dd'
+    ]);
+    ?>
+    </div>
+       </div>
+    <?= $form->field($model, 'propietario')->textInput(['value' => Yii::$app->user->identity->username, 'readonly' => true]) ?>
 
-    <?= $form->field($model, 'fecha_inicio')->textInput() ?>
 
-    <?= $form->field($model, 'fecha_fin')->textInput() ?>
-
-    <?= $form->field($model, 'propietario')->textInput(['value' => Yii::$app->user->identity->username, 'readonly'=>true]) ?>
-    
-    
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+<?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
 
-    <?php ActiveForm::end(); ?>
+<?php ActiveForm::end(); ?>
 
 </div>
